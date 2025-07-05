0c4da42dde649990db359724dce04400fda3b576
Heinz-Juergen Steinhoff <hsteinho@uos.de>
2025-07-05 17:18:33 +0200

    Update HP_commands_config.py

--- CAREST/HP_commands_config.py
+++ CAREST/HP_commands_config.py
@@ -18,6 +18,23 @@ ramp_down_time_bt (300 s) the plate is reset to base_temperature SV.
 
 For checking the proper function of the plate the temperature ramp 
 can be started and the actual temperature values (PV) are printed every 2 seconds.
+
+Copyright (c) 2025 Heinz-Juergen Steinhoff
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 '''
 
 import time
